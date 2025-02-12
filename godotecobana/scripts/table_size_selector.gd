a6dd3ddf3feea873a94d45347b9ba992f8b6e048
jac192133 <jac192133@gmail.com>
2025-02-11 21:32:54 -0500

    table tweak

--- godotecobana/scripts/table_size_selector.gd
+++ godotecobana/scripts/table_size_selector.gd
@@ -3,9 +3,15 @@ extends OptionButton
 @onready var table = get_node("/root/RoomScene/Table")
 
 func _ready():
-	# Correct way to connect signals in Godot 4
+	# Ensure the signal is connected properly
 	item_selected.connect(_on_item_selected)
 
 func _on_item_selected(index: int):
 	if table:
-		table.change_table_size(index)
+		# Get the TableSize enum from the table script
+		var table_enum = table.get("TableSize")  
+		if table_enum:
+			table.change_table_size(index)
+			print("Table size changed to:", table_enum.keys()[index])  # Debug output
+		else:
+			print("ERROR: TableSize enum not found in table.")
