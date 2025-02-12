7b734cad92920f245b00dd34e8baae7d708bf5ba
jac192133 <jac192133@gmail.com>
2025-02-11 21:45:29 -0500

    partial dropdownmenu re tables fix

--- godotecobana/scripts/table_size_selector.gd
+++ godotecobana/scripts/table_size_selector.gd
@@ -3,15 +3,14 @@ extends OptionButton
 @onready var table = get_node("/root/RoomScene/Table")
 
 func _ready():
-	# Ensure the signal is connected properly
+	clear()
+	add_item("Small Table")
+	add_item("Medium Table")
+	add_item("Large Table")
+	add_item("Extra-Long Table")
 	item_selected.connect(_on_item_selected)
 
-func _on_item_selected(index: int):
+func _on_item_selected(index):
 	if table:
-		# Get the TableSize enum from the table script
-		var table_enum = table.get("TableSize")  
-		if table_enum:
-			table.change_table_size(index)
-			print("Table size changed to:", table_enum.keys()[index])  # Debug output
-		else:
-			print("ERROR: TableSize enum not found in table.")
+		table.change_table_size(index)
+		print("Selected table size index:", index)
